d19ef2ac76308fe9b43ec43ab4966488ee1a9d00
ren_ren <rinya1608@gmail.com>
2023-04-01 11:32:53 +0500

    init spring boot

---
+++ src/main/java/com/ren/selfbusiness/SelfBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.ren.selfbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfBusinessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SelfBusinessApplication.class, args);
+
+    }
+
+}
