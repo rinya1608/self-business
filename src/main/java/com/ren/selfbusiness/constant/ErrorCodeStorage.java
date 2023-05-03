d48a08c21eedd997020bd4f01fba96fe732a30b4
ren_ren <re.yarullin@gmail.com>
2023-05-03 13:15:40 +0500

    add error for resourceType

--- src/main/java/com/ren/selfbusiness/constant/ErrorCodeStorage.java
+++ src/main/java/com/ren/selfbusiness/constant/ErrorCodeStorage.java
@@ -5,4 +5,5 @@ public class ErrorCodeStorage {
     public static final String UNKNOWN_01 = "UNKNOWN_01";
     public static final String REG_01 = "REG_01";
     public static final String REG_02 = "REG_02";
+    public static final String RT_01 = "RT_01";
 }
--- src/main/java/com/ren/selfbusiness/enumarate/ErrorDtoEnum.java
+++ src/main/java/com/ren/selfbusiness/enumarate/ErrorDtoEnum.java
@@ -10,7 +10,8 @@ public enum ErrorDtoEnum {
     AUTH_01_DTO(new ErrorDto(ErrorCodeStorage.AUTH_01, "Неверный email или пароль")),
     UNKNOWN_01_DTO(new ErrorDto(ErrorCodeStorage.UNKNOWN_01, "Неизвестная ошибка")),
     REG_01_DTO(new ErrorDto(ErrorCodeStorage.REG_01, "Пользователь с таким email уже существует")),
-    REG_02_DTO(new ErrorDto(ErrorCodeStorage.REG_02, "Некорректные данные"));
+    REG_02_DTO(new ErrorDto(ErrorCodeStorage.REG_02, "Некорректные данные")),
+    RT_01_DTO(new ErrorDto(ErrorCodeStorage.RT_01, "Тип с таким id не найден"));
 
     private final ErrorDto errorDto;
 
